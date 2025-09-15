14c1f3ce65bc14403ba6d1b297f36c55217ac824
deadsec07 <alihasnat07@gmail.com>
2025-09-15 22:00:54 +0530

    feat(site): Unseen Browser downloads page + CF/S3 deploy workflow hook

---
+++ site/gtm.js
@@ -0,0 +1,5 @@
+// gtm.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-S01M343L20', { anonymize_ip: true });
